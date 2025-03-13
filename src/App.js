66f24becaada2176ee3c2eb97efbbffb61568f2e
YSH-17 <mamidipallisrivyshnavi5@gmail.com>
2025-03-13 13:47:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//App.js
+import React from 'react';
+import './App.css';
+import CompressorComp 
+    from "./Components/Compressor";
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+    return (
+        <CompressorComp />
+    );
+}
+export default App;
+
